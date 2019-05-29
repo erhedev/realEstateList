86a4c36ae8ddd53fb378cfa3721e0ede3736fc8f
erhedev <erik.hede.dev@gmail.com>
2019-05-29 12:12:03 +0200

    Initial Commit

---
+++ realEstateList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  realEstateList
+//
+//  Created by Erik Hede on 29/05/2019.
+//  Copyright © 2019 Erik Hede. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ realEstateListTests/realEstateListTests.swift
@@ -0,0 +1,34 @@
+//
+//  realEstateListTests.swift
+//  realEstateListTests
+//
+//  Created by Erik Hede on 29/05/2019.
+//  Copyright © 2019 Erik Hede. All rights reserved.
+//
+
+import XCTest
+@testable import realEstateList
+
+class realEstateListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
